dc1a85eddf2252d4483ab0010d1dd29156451f0d
Vadim Kershukov <vadim.kershukov@softswiss.com>
2025-09-01 17:13:04 +0400

    prod url

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from config.settings import get_config
 from utils.logging import setup_logging, get_logger
 from fastmcp import FastMCP
-from fastmcp.server.auth.providers.workos import AuthKitProvider
+
 
 # Initialize configuration and logging
 config = get_config()
@@ -14,10 +14,6 @@ logger.info(
     config.intervals.base_url,
 )
 
-auth_provider = AuthKitProvider(
-    authkit_domain="https://sweet-portal-24-staging.authkit.app",
-    base_url="http://localhost:8002"
-)
 
 # Create FastMCP instance with unique name
-mcp = FastMCP("intervals-icu-mcp-server", auth=auth_provider)
+mcp = FastMCP("intervals-icu-mcp-server")
